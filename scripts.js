19f59ee0250aff6801eba413a9c3695a70271458
HostileGenius <135417455+HostileGenius@users.noreply.github.com>
2024-06-05 19:23:39 +0530

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,3 @@
 document.getElementById('downloadBtn').addEventListener('click', function() {
-    window.location.href = 'D:/'; // Replace with the actual file path
+    window.location.href = 'D:/'; 
 });
